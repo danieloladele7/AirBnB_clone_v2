60e5b7f4a8e575db31398513c63eb3a2d6e798da
Jason Le Roux <jasonleroux64@gmail.com>
2023-06-21 15:04:55 +0100

    task 3 in progress...

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,7 @@
 -- this script prepares a MySQL server for the project
 -- create project developement database with the name : hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- creating new user named : hbnb_dev with all privileges on the db hbnb_dev_db
+-- with the password : hbnb_dev_pwd if it dosen't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- granting all privileges to the new user
