e47904e777ed68ef95767e3102c7f268a315a1b4
Daniel <danieloladele7@gmail.com>
2023-06-25 00:50:11 +0200

    working on console do_create

--- console.py
+++ console.py
@@ -159,14 +159,13 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
             return
         if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-            if 'id' not in obj_kwargs:
+            if not hasattr(obj_kwargs, 'id'):
                 obj_kwargs['id'] = str(uuid.uuid4())
-            if 'created_at' not in obj_kwargs:
+            if not hasattr(obj_kwargs, 'created_at'):
                 obj_kwargs['created_at'] = str(datetime.now())
-            if 'updated_at' not in obj_kwargs:
+            if not hasattr(obj_kwargs, 'updated_at'):
                 obj_kwargs['updated_at'] = str(datetime.now())
             new_instance = HBNBCommand.classes[class_name](**obj_kwargs)
-            setattr(new_instance, "state_id", obj_kwargs.get("state_id", None))
             new_instance.save()
             print(new_instance.id)
         else:
