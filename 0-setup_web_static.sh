b504641671c5d9169b1bb52d7639f51f7b103f60
Daniel Oladele <danieloladele7@gmail.com>
2023-07-09 07:44:10 +0100

    setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -36,7 +36,7 @@ sudo cp /etc/nginx/sites-enabled/default nginx-sites-enabled_default.backup
 
 #--setup the content of /data/web_static/current/
 # to redirect to webcronx.tech/hbnb_static
-sudo sed -i '19i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 echo -e "\e[1;32m Symbolic link created\e[0m"
 echo
 
