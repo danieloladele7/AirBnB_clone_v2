52b61da34cb723d77f651cb825daf300204cc9fd
Daniel Oladele <danieloladele7@gmail.com>
2023-07-09 11:32:57 +0100

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,20 +6,19 @@
 - return archive path if successful. Otherwise, it should return None
 '''
 
-from fabric.api import local
 from datetime import datetime
+from fabric.api import local
+from os.path import isdir
 
 
 def do_pack():
-    """
-        generates a .tgz archine from contents of web_static
-    """
-    time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-    file_name = "versions/web_static_{}.tgz".format(time)
+    """generates a tgz archive"""
     try:
-        local("mkdir -p ./versions")
-        local("tar --create --verbose -z --file={} ./web_static"
-              .format(file_name))
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
         return file_name
     except:
         return None
